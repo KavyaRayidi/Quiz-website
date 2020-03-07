723ebf6dcc0972b8b5f519a3d05f0653708360ef
KavyaRayidi <55911680+KavyaRayidi@users.noreply.github.com>
2020-03-08 02:29:02 +0530

    Add files via upload
    This is a simple website that uses HTML, CSS, JavaScript to conduct a quiz .

---
+++ proscript.js
@@ -0,0 +1,98 @@
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("bt" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions
+var questions = [
+    new Question("What is the full form of RAM?", ["Random Access Memory", "Randomly Allocated Memory", "Routine Accessible Memory", "none"], "Random Access Memory"),
+
+    new Question("What is CSS used for?", ["To create web pages", "Security", "Styling", "All of the above"], "Styling"),
+
+
+    new Question("Which among the following is a single integrated circuit?", ["Chip", "Gate", "CPU", "Mother board"], "Gate"),
+
+
+    new Question("C is?", ["machine level language", "3rd generation language ", "Assembly language", "All of the above"], "All of the above"),
+
+
+];
+
+
+var quiz = new Quiz(questions);
+
+
+populate();
+
+
+
+
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
